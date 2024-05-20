28c6bed745aa9df4e22a1ecb2eb9a0ca3449df55
yunlinIT <yunlinit@gmail.com>
2024-05-20 14:16:58 +0900

    프로젝트생성

---
+++ src/main/java/com/koreait/sssandupload/SssAndUploadApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.sssandupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SssAndUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SssAndUploadApplication.class, args);
+    }
+
+}
