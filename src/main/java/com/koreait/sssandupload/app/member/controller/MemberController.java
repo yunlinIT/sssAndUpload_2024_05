6eb3b3a3194e2e4b7bf6dc8d9f9e9b18f46b1749
yunlinIT <yunlinit@gmail.com>
2024-05-20 16:53:18 +0900

    로그인 방식을 스프링 시큐리티로 변경

--- src/main/java/com/koreait/sssandupload/app/member/controller/MemberController.java
+++ src/main/java/com/koreait/sssandupload/app/member/controller/MemberController.java
@@ -3,18 +3,24 @@ package com.koreait.sssandupload.app.member.controller;
 import com.koreait.sssandupload.app.member.entity.Member;
 import com.koreait.sssandupload.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/join")
     public String showJoin() {
@@ -22,30 +28,32 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String join(String username, String password, String email, MultipartFile profileImg, HttpSession session) {
+    public String join(HttpServletRequest req, String username, String password, String email, MultipartFile profileImg) {
         Member oldMember = memberService.getMemberByUsername(username);
+
+        String passwordClearText = password;
+        password = passwordEncoder.encode(password);
 
         if (oldMember != null) {
             return "redirect:/?errorMsg=AlreadyExists";
         }
 
-        Member member = memberService.join(username, "{noop}" + password, email, profileImg);
+        Member member = memberService.join(username, password, email, profileImg);
 
-        session.setAttribute("loginedMemberId", member.getId());
+        try {
+            req.login(username, passwordClearText);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        }
 
         return "redirect:/member/profile";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/profile")
-    public String showProfile(HttpSession session, Model model) {
-        Long loginedMemberId = (Long) session.getAttribute("loginedMemberId");
-        boolean isLogined = loginedMemberId != null;
+    public String showProfile(Principal principal, Model model) {
 
-        if(isLogined == false) {
-            return "redirect:/?errorMsg=Need to Login";
-        }
-
-        Member loginedMember = memberService.getMemberById(loginedMemberId);
+        Member loginedMember = memberService.getMemberByUsername(principal.getName());
 
         model.addAttribute("loginedMember", loginedMember);
 
--- src/main/java/com/koreait/sssandupload/app/member/service/MemberService.java
+++ src/main/java/com/koreait/sssandupload/app/member/service/MemberService.java
@@ -4,16 +4,24 @@ import com.koreait.sssandupload.app.member.entity.Member;
 import com.koreait.sssandupload.app.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
     @Value("${custom.genFileDirPath}")
     private String genFileDirPath;
 
@@ -49,5 +57,15 @@ public class MemberService {
 
     public Member getMemberById(Long id) {
         return memberRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username).get();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("member"));
+
+        return new User(member.getUsername(), member.getPassword(), authorities);
     }
 }
