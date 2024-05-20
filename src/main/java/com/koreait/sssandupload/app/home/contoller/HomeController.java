1151d88b11197086bd77e2461a69d64ab327c0b7
yunlinIT <yunlinit@gmail.com>
2024-05-20 14:54:57 +0900

    업로드 폼

--- src/main/java/com/koreait/sssandupload/app/home/contoller/HomeController.java
+++ src/main/java/com/koreait/sssandupload/app/home/contoller/HomeController.java
@@ -11,4 +11,9 @@ public class HomeController {
     public String main() {
         return "home/main";
     }
+
+    @RequestMapping("/test/upload")
+    public String upload() {
+        return "home/test/upload";
+    }
 }
