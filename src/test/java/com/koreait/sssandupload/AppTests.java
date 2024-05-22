1bbf69141b0cd831ac94b4fb09a863ad91d5136c
yunlinIT <yunlinit@gmail.com>
2024-05-22 13:23:42 +0900

    GET 요청의 응답 본문엔 안녕이 나와야 함

---
+++ src/test/java/com/koreait/sssandupload/AppTests.java
@@ -0,0 +1,44 @@
+package com.koreait.sssandupload;
+
+import com.koreait.sssandupload.app.home.contoller.HomeController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import javax.transaction.Transactional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class AppTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("메인화면에서는 안녕이 나와야 함")
+    void t1() throws Exception {
+        // WHEN
+        // GET /
+        ResultActions resultActions = mvc
+                .perform(get("/"))
+                .andDo(print());
+
+        // THEN
+        // 안녕
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(HomeController.class))
+                .andExpect(handler().methodName("main"))
+                .andExpect(content().string(containsString("안녕")));
+    }
+}
