dc9b61895bd067c938339ee71e5b7095ec15b512
yunlinIT <yunlinit@gmail.com>
2024-05-22 13:34:50 +0900

    모든 액션마다 작성한 현재 로그인 한 회원의 정보 가져오는 로직 제거, 이미지 누락

--- src/main/java/com/koreait/sssandupload/app/home/contoller/HomeController.java
+++ src/main/java/com/koreait/sssandupload/app/home/contoller/HomeController.java
@@ -1,14 +1,9 @@
 package com.koreait.sssandupload.app.home.contoller;
 
-import com.koreait.sssandupload.app.member.entity.Member;
 import com.koreait.sssandupload.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,40 +11,12 @@ public class HomeController {
     private final MemberService memberService;
 
     @GetMapping("/")
-    public String showMain(Principal principal, Model model) {
-        Member loginedMember = null;
-        String loginedMemberProfileImgUrl = null;
-
-        if (principal != null && principal.getName() != null) {
-            loginedMember = memberService.getMemberByUsername(principal.getName());
-        }
-
-        if (loginedMember != null) {
-            loginedMemberProfileImgUrl = loginedMember.getProfileImgUrl();
-        }
-
-        model.addAttribute("loginedMember", loginedMember);
-        model.addAttribute("loginedMemberProfileImgUrl", loginedMemberProfileImgUrl);
-
+    public String showMain() {
         return "home/main";
     }
 
     @GetMapping("/about")
-    public String showAbout(Principal principal, Model model) {
-        Member loginedMember = null;
-        String loginedMemberProfileImgUrl = null;
-
-        if (principal != null && principal.getName() != null) {
-            loginedMember = memberService.getMemberByUsername(principal.getName());
-        }
-
-        if (loginedMember != null) {
-            loginedMemberProfileImgUrl = loginedMember.getProfileImgUrl();
-        }
-
-        model.addAttribute("loginedMember", loginedMember);
-        model.addAttribute("loginedMemberProfileImgUrl", loginedMemberProfileImgUrl);
-
+    public String showAbout() {
         return "home/about";
     }
 }
